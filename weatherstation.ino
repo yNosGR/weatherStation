4d044a4ab224ae3ba9f78301ed611e6e836bd6ba
Craig Bailey <ynos@ynos.us>
2017-10-12 16:47:32 -0400

    more cleaned up, time to add some comments

--- weatherstation.ino
+++ weatherstation.ino
@@ -44,15 +44,9 @@ void setup()  {
   /*
   Serial.println("clearing wifi settins");
   wifiManager.resetSettings();
-  */
+   */
   
   Serial.println("Beginning setup");
-  Serial.println("  Initializing arrays");
-  for ( int thisReading = 0 ; thisReading < numReadings; thisReading++) {
-    treadings[thisReading] = 0;
-    hreadings[thisReading] = 0;
-    preadings[thisReading] = 0;
-  }
 
   if (!bmp.begin()) {
     Serial.println("Could not find BMP180 or BMP085 sensor at 0x77");
@@ -77,70 +71,32 @@ void loop(){
   
   // simple altitude compensation. 
   p = (p/100)+26 ; 
-
-  htotal = htotal - hreadings[readIndex];
-  hreadings[readIndex] = h;
-  htotal = htotal + hreadings[readIndex];
-  haverage = htotal / numReadings;
-  
-  ttotal = ttotal - treadings[readIndex];
-  treadings[readIndex] = t;
-  ttotal = ttotal + treadings[readIndex];
-  taverage = ttotal / numReadings;
-
-  ptotal = ptotal - preadings[readIndex];
-  preadings[readIndex] = p;
-  ptotal = ptotal + preadings[readIndex];
-  paverage = ptotal / numReadings;
-  
-  readIndex = readIndex + 1;
-
-  if (readIndex >= numReadings) {
-    readIndex = 0;
-    if (isnan(haverage) || isnan(taverage)) {
-      Serial.println("humudity and/or temp averages are NaN");
-      ESP.reset ();
-    }
-    Serial.print("Average Temperature: ");
-    Serial.print(taverage);
-    Serial.print(" Average humidity: ");
-    Serial.print(haverage);
-    Serial.print(" pressure in hPa: ");
-    Serial.println(paverage);
-    Serial.println("Sending data to Thingspeak");
     
-    if (client.connect(server,80)) {
-      String postStr = apiKey;
-      postStr +="&field1=";
-      postStr += String(taverage);
-      postStr +="&field2=";
-      postStr += String(haverage);
-      postStr +="&field3=";
-      postStr += String(paverage);
-      postStr += "\r\n\r\n";
-       
-      client.print("POST /update HTTP/1.1\n");
-      client.print("Host: api.thingspeak.com\n");
-      client.print("Connection: close\n");
-      client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-      client.print("Content-Type: application/x-www-form-urlencoded\n");
-      client.print("Content-Length: ");
-      client.print(postStr.length());
-      client.print("\n\n");
-      client.print(postStr);
-       
-      
-    }
-    client.stop();
+  if (client.connect(server,80)) {
+    String postStr = apiKey;
+    postStr +="&field1=";
+    postStr += String(t);
+    postStr +="&field2=";
+    postStr += String(h);
+    postStr +="&field3=";
+    postStr += String(p);
+    postStr += "\r\n\r\n";
+     
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+     
+    
   }
+  client.stop();
 
-  
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor! Sleeping for a moment.");
-    delay(20000);
-    return;
-  }
-  
+
   if (isnan(p) ) {
     Serial.println("Failed to read from BMP180! Sleeping for a moment.");
     delay(20000);
